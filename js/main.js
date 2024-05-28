ed850040329f059a8247ae35701dae761db47fac
pai9nka <pai9nka@gmail.com>
2024-05-28 22:34:54 +0500

    resize header

--- js/main.js
+++ js/main.js
@@ -2,14 +2,22 @@ const headerEl = document.getElementById("header")
 
 window.addEventListener("scroll", function () {
   const scrollPos = window.scrollY
-
-  if (scrollPos > 100) {
+    if (scrollPos > 100) {
     headerEl.classList.add("header_mini")
   } else {
     headerEl.classList.remove("header_mini")
   }
-  console.log(headerH)
-  
+  /*console.log(headerH)*/
   /*document.getElementsByClassName('header')[0].style.height */
-})
+});
 
+window.addEventListener('resize', function() {
+  const  headerH   = headerEl.offsetHeight
+  if (headerH > 125) {
+    headerEl.classList.add("header_mini")
+  } else {
+    headerEl.classList.remove("header_mini")
+  }
+
+  console.log(headerH);
+});
